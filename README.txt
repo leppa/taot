50c5110227b5beb3440e32ed432c4b58c132d52c
Oleksii Serdiuk <contacts@oleksii.name>
2013-01-26 20:08:57 +0100

    Added README, license and copyright informatrion.

---
+++ README.txt
@@ -0,0 +1,30 @@
+================================
+ The Advanced Online Translator
+================================
+
+Copyright (C) 2013 Oleksii Serdiuk <contacts[at]oleksii[dot]name>
+
+The Advanced Online Translator uses available online translation
+services to provide translations. Currently it supports only Google
+Translate but more services are in the plans (i.e., Bing Translate,
+Yandex Translate, etc.).
+
+For Google Translate alternative and reverse translations are displayed
+for single words.
+
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+$Id: $Format:%h %ai %an$ $
--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
+/*
+ *  The Advanced Online Translator.
+ *  Copyright (C) 2013  Oleksii Serdiuk <contacts[at]oleksii[dot]name>
+ *
+ *  $Id: $Format:%h %ai %an$ $
+ *
+ *  This file is part of The Advanced Online Translator.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <QApplication>
 #include "qmlapplicationviewer.h"
 
 int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
-    
+
     QmlApplicationViewer viewer;
     viewer.addImportPath(QLatin1String("modules")); // ADDIMPORTPATH
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto); // ORIENTATION
     viewer.setMainQmlFile(QLatin1String("qml/main.qml")); // MAINQML
     viewer.showExpanded();
-    
+
     return app->exec();
 }
-
